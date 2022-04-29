fa1c9091feed1d5150a72d5145338dc8df24c1f9
rafamamani <104235071+rafamamani@users.noreply.github.com>
2022-04-28 21:06:39 -0300

    ejercicioMedalla ultima revision

--- src/ejercicio11.ts
+++ src/ejercicio11.ts
@@ -6,15 +6,27 @@ Tenga en cuenta que para el primer puesto se entrega medalla de oro, segundo
 puesto medalla de plata y tercer puesto medalla de bronce. En caso que quede
 en otra posición se entrega certificado de participación.
 *-----------------------------------------------------------------------------*/
-let posicion: number = Number(prompt("Ingresa el resultados de la carrera"));
-if (posicion ===1) {
-  console.log("Entregar medalla de ORO");
+let posicionLlegada: number = Number(
+  prompt("Ingresa el resultados de la carrera")
+);
+//verifico ingreso de numero positivo
+if (posicionLlegada < 1) {
+  console.log("ingreso una posicion invalida");
 } else {
-  if (posicion ===2) {
-    console.log("Entregar medalla de PLATA");
-} else{
-  if (posicion ===3) {
-    console.log("Entregar medalla de BRONCE");
-} else{
-  console.log("Entregar certificado de PARTICIPACION");
+  //verificacion para  entrega medalla de ORO
+  if (posicionLlegada === 1) {
+    console.log("Entregar medalla de ORO");
+  } else {
+    //verificacion para  entrega medalla de PLATA
+    if (posicionLlegada === 2) {
+      console.log("Entregar medalla de PLATA");
+    } else {
+      //verificacion para  entrega medalla de BRONCE
+      if (posicionLlegada === 3) {
+        console.log("Entregar medalla de BRONCE");
+      } else {
+        console.log("Entregar certificado de PARTICIPACION");
+      }
+    }
+  }
 }
